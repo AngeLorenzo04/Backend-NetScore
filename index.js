7242ce47cf08ae85e4a26a56fb85c0d863e0fb6c
AngeLorenzo04 <dicandiangelorenzo@gmail.com>
2026-06-24 10:02:18 +0200

    refactor: move socket logic to external manager and update server startup scripts

--- index.js
+++ index.js
@@ -6,17 +6,5 @@ server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-io.on('connection', (socket) => {
-  console.log('A user connected:', socket.id);
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected:', socket.id);
-  });
-
-  // Example: Listen for a custom event
-  socket.on('chatMessage', (msg) => {
-    console.log('message: ' + msg);
-    io.emit('chatMessage', msg); // Broadcast to all connected clients
-  });
-});
+// Server listens, socket.io is initialized inside src/app.js via socketManager
 
